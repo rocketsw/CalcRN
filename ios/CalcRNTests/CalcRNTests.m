25d3c69d218656eef4da97ea8620d5d0c39f38a4
rocketsw <thombaker@gmail.com>
2024-08-04 15:04:50 -0400

    Changed CalcTS to CalcRN everywhere

--- ios/CalcRNTests/CalcRNTests.m
+++ ios/CalcRNTests/CalcRNTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CalcTSTests : XCTestCase
+@interface CalcRNTests : XCTestCase
 
 @end
 
-@implementation CalcTSTests
+@implementation CalcRNTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
